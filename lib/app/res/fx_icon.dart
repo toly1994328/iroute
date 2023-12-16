b04b138b5318959f6c0fe5ec1c51c6dc99d5ff80
toly <1981462002@qq.com>
2023-12-16 13:35:49 +0800

    books

---
+++ lib/app/res/fx_icon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+// Power By 张风捷特烈--- Generated file. Do not edit.
+// 欢迎支持： https://github.com/toly1994328/FlutterUnit
+class FxIcon {
+    FxIcon._();
+    static const IconData icon_sun = IconData(0xe61a, fontFamily: "FxIcon");
+static const IconData icon_anima = IconData(0xe66b, fontFamily: "FxIcon");
+static const IconData icon_scroll = IconData(0xe72c, fontFamily: "FxIcon");
+static const IconData icon_paint = IconData(0xe635, fontFamily: "FxIcon");
+static const IconData icon_layout = IconData(0xe619, fontFamily: "FxIcon");
+static const IconData icon_dream = IconData(0xe600, fontFamily: "FxIcon");
+static const IconData dashboard = IconData(0xe7dd, fontFamily: "FxIcon");
+static const IconData fill_shoushi = IconData(0xe69f, fontFamily: "FxIcon");
+
+}  
---
+++ packages/idraw/lib/idraw.dart
@@ -0,0 +1,13 @@
+library idraw;
+
+export 'p01/p01.dart';
+export 'p02/p02.dart';
+export 'p03/p03.dart';
+export 'p04/p04.dart';
+export 'p05/p05.dart';
+export 'p06/p06.dart';
+export 'p07/p07.dart';
+export 'p08/p08.dart';
+export 'p09/p09.dart';
+export 'p10/p10.dart';
+export 'p11/p11.dart';
